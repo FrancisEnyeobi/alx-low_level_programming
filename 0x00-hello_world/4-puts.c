86bb7f03d46c9b180d4b42775c5df942e5044bef
Francis Enyeobi <fc.enyeobi@unizik.edu.ng>
2022-10-13 21:01:12 -0700

    great

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
- */
+ *  * main - Entry
+ *   * Return: Always 0
+ *    */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-        return (0);
-}}
+	puts("\"Programming is like building a multilingual puzzle");
+	 return (0);
+}
+

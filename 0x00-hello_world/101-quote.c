68c309f91f61ef3e18f87fa67f00c801154f9623
Francis Enyeobi <fc.enyeobi@unizik.edu.ng>
2022-10-13 22:07:47 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
- *  *main-program entry point.
- *   *
- *    *Return: 1 on success.
- *     */
+ * main - Entry
+ * Return: 1
+ */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }

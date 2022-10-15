9c4226b755474ba34bcb3af02135e8912a84ad43
Francis Enyeobi <fc.enyeobi@unizik.edu.ng>
2022-10-14 20:17:39 -0700

    C variables Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,16 +9,16 @@ int main(void)
 
 	for (num = '0'; num <= '9'; num++)
 	{
-		  putchar(num);
-		  if (num == '9')
-			  {
-				   break;
-				  }
-		  else
-			  {
-				   putchar(',');
-				   putchar(' ');
-				  }
+		putchar(num);
+		if (num == '9')
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);

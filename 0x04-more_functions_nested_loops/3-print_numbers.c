88aa0a212bc340af2fe8a3908977852cd71c7b42
Francis Enyeobi <fc.enyeobi@unizik.edu.ng>
2022-10-21 07:03:50 -0700

    done

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
- * * print_numbers - print 0123456789
+ * print_numbers - print 0123456789
  * Return: void
  */
 void print_numbers(void)
 {
-	char c
-		;
+	char c;
+	
 	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
 	_putchar('\n');
-}
 }

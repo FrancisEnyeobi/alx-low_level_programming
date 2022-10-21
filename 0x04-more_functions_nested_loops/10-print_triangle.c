0953767907714f237f91b683114ae168d741be89
Francis Enyeobi <fc.enyeobi@unizik.edu.ng>
2022-10-21 07:45:49 -0700

    good

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_triangle - entry point
+ * Description: Prints diagonals
+ * @size: size of the triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int row, hashes, spaces;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		{
+			else
+		}
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}

053fd33a0df643a13679da06472a928de00ba05f
Francis Enyeobi <fc.enyeobi@unizik.edu.ng>
2022-11-10 18:49:52 -0800

    settled

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -20,9 +20,20 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		p = malloc(new_size);
 		return (p);
 	}
-	else if (new_size == 0
-			{
-			free(ptr);
-			return (NULL);
-			}
-			else if (new_size == old_size)
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (new_size == old_size)
+		return (ptr);
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	if (new_size > old_size)
+		n = old_size;
+	for (i = 0; i < n; i++)
+		p[i] = oldp[i];
+	free(ptr);
+	return (p);
+}
